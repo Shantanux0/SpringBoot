bd154a62c4c4972c52c6cd92e516f2370cf63ae2
Shanatanu Kale <kaleshantanu2260@gmail.com>
2025-02-04 21:14:34 +0530

    Delete SecurityFilterChain directory

--- SecurityFilterChain/SecurityAuth/src/main/java/com/SecurityApp/SecurityAuth/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.SecurityApp.SecurityAuth.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
